6183b0654e31d5fab0a65fa6d0c755ecfca0075a
porowns <porowns@gmail.com>
2019-09-18 15:16:39 -0400

    Add custom loadable url_slug

--- django_eveonline_connector/apps.py
+++ django_eveonline_connector/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class DjangoEveOnlineConnectorConfig(AppConfig):
     name = "django_eveonline_connector"
-    verbose_name = "eveonline"
+    verbose_name = "EVE Online Connector"
+    url_slug = 'eveonline'
