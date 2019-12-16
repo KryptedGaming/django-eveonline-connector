27bff72052578c06e293e371d48ec03a8aa2981d
porowns <porowns@gmail.com>
2019-12-16 16:11:46 -0500

    Update install script

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 mkdir -p /opt/eveonline/static/
-mv /sqlite-latest.sqlite.bz2 /opt/eveonline/static/
+cp /sqlite-latest.sqlite.bz2 /opt/eveonline/static/
 apt-get install -y bzip2
 bunzip2 /opt/eveonline/static/sqlite-latest.sqlite.bz2
 python3 ${manage_py} loaddata initial_scopes 
