57c982794565eac170f41bf1100f59963378aa95
porowns <porowns@gmail.com>
2020-09-29 10:52:16 -0400

    Remove fixture installation dependency

--- install.sh
+++ install.sh
@@ -2,4 +2,3 @@ mkdir -p /opt/eveonline/static/
 cp /sqlite-latest.sqlite.bz2 /opt/eveonline/static/
 apt-get install -y bzip2
 bunzip2 /opt/eveonline/static/sqlite-latest.sqlite.bz2
-python3 ${manage_py} loaddata initial_scopes 
